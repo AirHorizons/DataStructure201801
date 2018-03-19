f37cbce51575ce4d4c2b0b399f98193e086e8a13
Airhorizons <guardian1248@martini.snucse.org>
2018-03-19 15:45:54 +0900

    Create LList interface file

---
+++ Practice_LinkedList/LListInterface.java
@@ -0,0 +1,8 @@
+public interface LListInterface{
+  public boolean isEmpty();
+  public int size();
+  public void add(int index, Object item);
+  public void remove(int index);
+  public Object get(int index);
+  public void removeAll();
+}
