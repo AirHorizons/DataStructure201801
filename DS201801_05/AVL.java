f153d62f73ba405fb098bdac8a09e115ce415572
HS_Choi <awesomeiscat@gmail.com>
2018-05-17 17:49:06 +0900

    Add skeleton code to AVL

---
+++ DS201801_05/AVL.java
@@ -0,0 +1,18 @@
+class AVL<T extends Comparable<T>> {
+  tNode<T> root;
+  
+  public boolean isEmpty() { return root==null; }
+  public tNode<T> insert(T item) {
+
+  }
+  public tNode<T> retrieve(T key) {
+
+  }
+  public tNode<T> delete(item T) {
+    
+  }
+  private tNode<T> deleteItem(T key, tNode<T> Node) {
+
+  }
+
+}
