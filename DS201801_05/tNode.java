39a25f2b8a9563e723b788f20d33389d2742306a
HS_Choi <awesomeiscat@gmail.com>
2018-05-17 17:28:51 +0900

    Add treeNode

---
+++ DS201801_05/tNode.java
@@ -0,0 +1,23 @@
+class tNode<T> {
+  private T item;
+  private tNode<T> left;
+  private tNode<T> right;
+
+  public tNode(T item, tNode<T> left, tNode<T> right) {
+    this.item = item;
+    this.left = left;
+    this.right = right;
+  }
+  public tNode(T item) {
+    this(item, null, null);
+  }
+  public tNode() {
+    this(null, null, null);
+  }
+  public T getItem() { return this.item; }
+  public void setItem(I item) { this.item = item; }
+  public tNode<T> getLeft() { return this.left; }
+  public void setLeft(tNode<T> left) { this.left = left; }
+  public tNode<T> getRight() { return this.right; }
+  public void setRight(tNode<T> right) { this.right = right; }
+}
