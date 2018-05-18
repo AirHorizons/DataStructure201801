6b7002d0705f88b26c07ad5634767ae6fb5d4a34
HS_Choi <awesomeiscat@gmail.com>
2018-05-18 15:57:29 +0900

    Create Parent BST class for AVL

--- DS201801_05/AVL.java
+++ DS201801_05/AVL.java
@@ -1,18 +1,4 @@
 class AVL<T extends Comparable<T>> {
-  tNode<T> root;
+  int height;
   
-  public boolean isEmpty() { return root==null; }
-  public tNode<T> insert(T item) {
-
-  }
-  public tNode<T> retrieve(T key) {
-
-  }
-  public tNode<T> delete(item T) {
-    
-  }
-  private tNode<T> deleteItem(T key, tNode<T> Node) {
-
-  }
-
 }
---
+++ DS201801_05/BST.java
@@ -0,0 +1,52 @@
+class BST<T extends Comparable<T>> {
+  tNode<T> root;
+  
+  public boolean isEmpty() { return root==null; }
+  
+  // Insert
+  //------------------------------------------------//
+  public tNode<T> insert(T item) {
+    return insertItem(root, item);
+  }
+  protected tNode<T> insertItem(tNode Node, T item) {
+    if (Node == null) {
+      Node = new Node(item);
+    }
+    else if (item.compareTo(Node.getItem()) < 0) {
+      Node.setLeft(insertItem(Node.getLeft(), item));
+    }
+    else if (item.compareTo(Node.getItem()) > 0) {
+      Node.setRight(insertItem(Node.getRight(), item));
+    }
+    return Node;
+  }
+  // Retrieve
+  //------------------------------------------------//
+  public tNode<T> retrieve(T item) {
+    return retrieveItem(root, item);
+  }
+  protected tNode<T> retrieveItem(tNode Node, T item) {
+    if (Node == null) return null;
+    else {
+      if (item.equals(Node.getItem())) return Node;
+      else if (item.compareTo(Node.getItem()) < 0)
+        return retrieveItem(Node.getLeft(), item);
+      else 
+        return retrieveItem(Node.getRight(), item);
+    }
+  }
+  // Delete
+  //------------------------------------------------//
+  public tNode<T> delete(T item) throws ItemNotFoundException {
+    try {
+      return deleteItem(root, item);
+    }
+    catch (ItemNotFoundException e) {
+      throw e;
+    }
+  } 
+  protected tNode<T> deleteItem(tNode Node, T item) {
+
+  }
+
+}
