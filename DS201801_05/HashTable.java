fbd03a8c8d80cdd030813a9cf86e0f27c45f60ba
HS_Choi <awesomeiscat@gmail.com>
2018-05-24 22:14:46 +0900

    Implement HashTable

---
+++ DS201801_05/HashTable.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class HashTable<Key extends Comparable<Key>, Value> {
+  private ArrayList<AVL<Key, Value>>  Buckets;
+  private final int HASHSIZE = 100;
+
+  public HashTable() {
+    Buckets = new ArrayList<AVL<Key, Value>>(HASHSIZE);
+  }
+
+  public void insert(Key key, Value value) {
+    Buckets.get(getHash(key)).insert(key, value);
+  }
+
+  public LinkedList<Value> retrieve(Key key) {
+    tNode<Key, LinkedList<Value>> Node = Buckets.get(getHash(key)).retrieve(key);
+    if (Node != null) return Node.getValue();
+    else return null;
+  }
+
+  public int getHash(Key key) {
+    int length = key.toString().length();
+    int result = 0;
+    for (int i=0; i< ((length < 6) ? length : 6); i++) {
+      result += (int)key.toString().charAt(i);
+    }
+    return result % HASHSIZE;
+  }
+}
+
