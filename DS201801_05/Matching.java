8265476911fe814ee7f8b1e2a5cd88c442cb5840
HS_Choi <awesomeiscat@gmail.com>
2018-05-25 13:22:32 +0900

    Implement command method and sksleton for each commands

--- DS201801_05/Matching.java
+++ DS201801_05/Matching.java
@@ -4,8 +4,8 @@ import java.util.regex.Matcher;
 
 public class Matching
 {
-  private final String input_pattern = "([<@?]) (*)";
-  private Pattern p = Pattern.compile(input_pattern);
+  private final static String input_pattern = "([<@?]) (.*)";
+  private static Pattern p = Pattern.compile(input_pattern);
 
   private HashTable<String, StringPosition> ht;
 
@@ -37,6 +37,57 @@ public class Matching
     while (m.find()) {
       String command = m.group(1);
       String target = m.group(2); 
+      
+      if (command.equals("<")) {
+        readFile(target);
+      }
+      else if (command.equals("@")) {
+        printHash(target);
+      }
+      else if (command.equals("?")) {
+        printPattern(target);
+      }
     }
 	}
+  
+  private static void readFile(String target) {
+    FileInputStream fis = null;
+    BufferedReader br = null;
+    try {
+      fis = new FileInputStream(target);
+      br = new BufferedReader(new InputStreamReader(fis));
+
+      String line = br.readLine();
+      while (line != null) {
+        int length = line.length();
+        // TODO : take 6 characters and insert AVL trees
+      }
+    }
+    catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    finally {
+      try {
+        br.close();
+        fis.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  } 
+  private static void printHash(String target) {
+    int index = Integer.ParseInt(target);
+    if (ht == null) return;
+    
+    ht.get(index).preorder();
+  }
+  private static void printPattern(String target) {
+    // TODO
+  }
+  
+
 }
